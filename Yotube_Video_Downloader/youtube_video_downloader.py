17fc1cb3fac6eb7bd4a59b1fb41fd28c291ec72e
Ujwal <ujwalujwalc@gmail.com>
2022-10-28 18:14:19 +0530

    Updated youtube video downloader file

--- Yotube_Video_Downloader/youtube_video_downloader.py
+++ Yotube_Video_Downloader/youtube_video_downloader.py
@@ -9,7 +9,7 @@ def download(x):
         print("Video Downloading")
         print()
         youtube = pytube.YouTube(video_url)
-        video = youtube.streams.filter(progressive=True, file_extension='mp4').order_by(
+        youtube.streams.filter(progressive=True, file_extension='mp4').order_by(
             'resolution').desc().first().download(path_to_store)
         print("Video downloaded")
     except:
@@ -19,17 +19,10 @@ def download(x):
         txt_file.close()
 
 
+# Enter the location for the video to be downloaded
 path_to_store = "J:\My Drive\Research\Videos"
-path_to_csv = 'url-list.csv'
 
-data = pd.read_csv(path_to_csv)
+# Enter the URL of the video.
+URL = ""
 
-url_list = data["URL"].values.tolist()
-
-# this was my assigned list, comment this line as well as change mylist to url_list to get video of all the files
-mylist = url_list[51:77]
-
-for i in mylist:  # change it to url_list to get all videos in file
-    download(str(i))
-
-print()
+download(URL)
